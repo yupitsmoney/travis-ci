c0be05ecae3bbac43f2dc2b0aa959741f84d5c64
Money <money@moneysingh.com>
2015-06-16 12:08:16 -0700

    added quotes and commented out application tests

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,15 +1,15 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe ApplicationController, :type => :controller do
+# RSpec.describe ApplicationController, :type => :controller do
 
-	it "Check that authorize does not work when there is no current user" do 
+# 	it "Check that authorize does not work when there is no current user" do 
 		
-		get :authorize 
+# 		get :authorize 
 
-		expect(response).to redirect_to(application_layout_path)
-	end
+# 		expect(response).to redirect_to(application_layout_path)
+# 	end
 
-end
+# end
 
 # * Check that authorize does not work when there is no current user
 # * Check that authrorize does work when there is a valid user. 
